4934bcf9b4f89580967cc568536ab40c16eb26e6
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2021-12-14 12:54:55 +0100

    I fix the position of the card so that they are responsive in main and home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,20 +5,20 @@ const Home = () => {
   return (
     <section className='home-section'>
       <h1 className='home-section__title'>Últimas Ofertas</h1>
-      <article className='home-article__nintendo'>
-        <h2 className='home-article__title'>Nintendo</h2>
+      <article className='home-nintendo'>
+        <h2 className='home-title'>Nintendo</h2>
         <Card max={3} game='Nintendo' />
       </article>
-      <article className='home-article__playstation'>
-        <h2 className='home-article__title'>Playstation</h2>
+      <article className='home-playstation'>
+        <h2 className='home-title'>Playstation</h2>
         <Card max={3} game='Playstation' />
       </article>
-      <article className='home-article__xbox'>
-        <h2 className='home-article__title'>Xbox</h2>
+      <article className='home-xbox'>
+        <h2 className='home-title'>Xbox</h2>
         <Card max={3} game='Xbox' />
       </article>
-      <article className='home-article__pc'>
-        <h2 className='home-article__title'>PC</h2>
+      <article className='home-pc'>
+        <h2 className='home-title'>PC</h2>
         <Card max={3} game='PC' />
       </article>
     </section>
